08b2fe2d06a98990f47d300db26269dff8c6a1c4
Simon Vogensen <sbv@lakeside.dk>
2013-12-30 23:27:08 +0100

    added deletable filters. filters can now be combined.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('bidApp')
     .controller('MainCtrl', ['$scope', 'Data', '$log', 'ejsResource', function ($scope, Data, $log, ejsResource) {
+
       $scope.messages = Data.filterData('2012');
       $scope.period = Data.periodData();
       $scope.type = Data.typeData();
@@ -11,36 +12,78 @@ angular.module('bidApp')
       });
 
 
-
-
-
       // point to your ElasticSearch server
       var ejs = ejsResource('http://localhost:9200');
       var index = 'system1';
       var type = 'book';
 
+      var interval = 'year';
+      var rangeFacetFilter;
+      var termsFacetFilter;
+
       // setup the indices and types to search across
       // define our search function that will be called when a user
       // submits a search
-      ejs.Request().indices(index).types(type)
-          .facet(ejs.TermsFacet('types').field('type'))
-          .facet(ejs.DateHistogramFacet('period').field('created').interval('year')).doSearch(function(results){
-            $scope.results = results;
-            $log.log(results);
-          });
+      function doSearch() {
+        var termsFacet = ejs.TermsFacet('types').field('type');
+        var dataHistogramFacet = ejs.DateHistogramFacet('period').field('created').interval(interval);
 
-      $scope.filterPeriod = function (type1, intervalBucket) {
-        $log.log(type1+' '+intervalBucket);
-        var fromTS = new Date(intervalBucket);
-        var toTS = new Date(fromTS.getFullYear() + 1, fromTS.getMonth(), fromTS.getDate()).getTime();
-        var rangeFilter = ejs.RangeFilter('created').gte(intervalBucket).lt(toTS);
+        if (termsFacetFilter && rangeFacetFilter) {
+          var andFacet = ejs.AndFilter([termsFacetFilter, rangeFacetFilter]);
+          termsFacet.facetFilter(andFacet);
+          dataHistogramFacet.facetFilter(andFacet);
+        }
+        else if (termsFacetFilter) {
+          termsFacet.facetFilter(termsFacetFilter);
+          dataHistogramFacet.facetFilter(termsFacetFilter);
+        }
+        else if (rangeFacetFilter) {
+          termsFacet.facetFilter(rangeFacetFilter);
+          dataHistogramFacet.facetFilter(rangeFacetFilter);
+        }
+
         ejs.Request().indices(index).types(type)
-            .facet(ejs.TermsFacet('types').field('type').facetFilter(rangeFilter))
-            .facet(ejs.DateHistogramFacet('period').field('created').interval('month').facetFilter(rangeFilter)).doSearch(function(results){
+            .facet(termsFacet)
+            .facet(dataHistogramFacet).doSearch(function (results) {
               $scope.results = results;
               $log.log(results);
             });
+      }
 
+      doSearch();
+
+      $scope.removeSelectedYear = function () {
+        $scope.yearSelected = null;
+        rangeFacetFilter = null;
+        interval = 'year';
+        doSearch();
+      };
+
+      $scope.removeSelectedType = function () {
+        $scope.typeSelected = null;
+        termsFacetFilter = null;
+        doSearch();
+      };
+
+      $scope.filterPeriod = function (name, intervalBucket) {
+        if(!$scope.yearSelected) {
+          $log.log(name+' '+intervalBucket);
+          var fromTS = new Date(intervalBucket);
+          var toTS = new Date(fromTS.getFullYear() + 1, fromTS.getMonth(), fromTS.getDate()).getTime();
+          rangeFacetFilter = ejs.RangeFilter('created').gte(intervalBucket).lt(toTS);
+          interval = 'month';
+          $scope.yearSelected = fromTS.getFullYear();
+          doSearch();
+        }
+      };
+
+      $scope.filterType = function (name, term) {
+        if(!$scope.typeSelected) {
+          $log.log(name+' '+term);
+          termsFacetFilter = ejs.TermsFilter('type', term);
+          $scope.typeSelected = term;
+          doSearch();
+        }
       };
 
       function getRandomInt (min, max) {
